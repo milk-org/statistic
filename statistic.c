635aff33796e8a2cea83e2f2a61d2979a6992044
oguyon <oliv.guyon@gmail.com>
2020-04-21 19:57:21 -1000

    remove redundant module name def

--- statistic.c
+++ statistic.c
@@ -23,8 +23,6 @@
 // Module short description
 #define MODULE_DESCRIPTION       "Statistics functions and tools"
 
-// Application to which module belongs
-#define MODULE_APPLICATION       "milk"
 
 
 
